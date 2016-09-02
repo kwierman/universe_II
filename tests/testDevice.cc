f271ade4a7536c0d824308d1d6ed820c563d4af5
Kevin Wierman <kwierman@gmail.com>
2016-09-02 14:41:28 -0400

    Unit Testing

--- tests/testDevice.cc
+++ tests/testDevice.cc
@@ -1,11 +1,11 @@
 #include <iostream>
 
-#include "TUVMEDMADevice.hh"
+#include "TUVMEDevice.hh"
 
 
 int main(){
 
-    TUVMEDMADevice mydevice(uint32_t(0));
+    TUVMEDevice mydevice(uint32_t(0));
     mydevice.Open();
 
 }
