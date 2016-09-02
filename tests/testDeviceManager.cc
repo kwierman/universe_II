3d4354a15b980d201141ddd0a0ed11904f5a3a32
Kevin Wierman <kwierman@gmail.com>
2016-09-02 15:39:06 -0400

    Unit Testing

--- tests/testDeviceManager.cc
+++ tests/testDeviceManager.cc
@@ -1,4 +1,12 @@
+#include <TUVMEDeviceManager.h>
+#include <iostream>
 
 int main(){
+
+    std::cout<<"Testing Device Manager"<<std::endl;
+
+    TUVMEDeviceManager* man = TUVMEDeviceManager::GetDeviceManager();
+
+
 
 }
