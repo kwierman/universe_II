99719b9fb38b0650fdf01318c7be04bc33964c63
Kevin Wierman <kwierman@gmail.com>
2016-09-02 15:43:00 -0400

    Unit Testing

--- tests/testDeviceManager.cc
+++ tests/testDeviceManager.cc
@@ -1,4 +1,5 @@
 #include <TUVMEDeviceManager.hh>
+#include <TUVMEControlDevice.hh>
 #include <iostream>
 
 int main(){
@@ -6,7 +7,11 @@ int main(){
     std::cout<<"Testing Device Manager"<<std::endl;
 
     TUVMEDeviceManager* man = TUVMEDeviceManager::GetDeviceManager();
+    std::cout<<"Getting Control Device"<<std::endl;
+
+    TUVMEControlDevice* con = man->GetControlDevice();
+    std::cout<<"\t Control Device"<<con<<std::endl;
 
 
-
+    std::cout<<"Done"<<std::endl;
 }
