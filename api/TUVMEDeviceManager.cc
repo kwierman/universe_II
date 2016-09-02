ce92175e25599aa92bc6b532902f052a9aac7fb4
Kevin Wierman <kwierman@gmail.com>
2016-09-02 15:24:09 -0400

    Unit Testing

--- api/TUVMEDeviceManager.cc
+++ api/TUVMEDeviceManager.cc
@@ -15,6 +15,7 @@ TUVMEDeviceManager::TUVMEDeviceManager()
   fUsePostedWrites = false;
   if (fControlDevice.Open() < 0) {
     /* Error, also clear out the available devices. */
+    std::cout<<"Cannot Open Control Device"<<std::endl;
     fControlDeviceIsOpen = false;
     fDevicesRemaining.clear();
   } else {
@@ -22,7 +23,7 @@ TUVMEDeviceManager::TUVMEDeviceManager()
     fControlDeviceIsOpen = true;
     uint32_t buffer = 0;
     if (fControlDevice.Read((char*)&buffer, sizeof(uint32_t), PCI_CSR)==sizeof(uint32_t)) {
-      /* This sets the Bus Master bit in the PCI_CSR register of the Universe II. 
+      /* This sets the Bus Master bit in the PCI_CSR register of the Universe II.
        * This is needed to complete DMA transfers. */
       buffer |= 0x4;
       fControlDevice.Write((char*)&buffer, sizeof(uint32_t), PCI_CSR);
@@ -42,15 +43,15 @@ TUVMEDeviceManager::TUVMEDeviceManager()
 
 TUVMEDeviceManager::~TUVMEDeviceManager()
 {
-  std::set<TUVMEDevice*>::iterator iter; 
+  std::set<TUVMEDevice*>::iterator iter;
   LockDevice();
-  for (iter = fAllDevices.begin();iter != fAllDevices.end(); iter++) {  
+  for (iter = fAllDevices.begin();iter != fAllDevices.end(); iter++) {
     delete *iter;
   }
   UnlockDevice();
 }
 
-TUVMEDevice* TUVMEDeviceManager::GetDevice(uint32_t vmeAddress, 
+TUVMEDevice* TUVMEDeviceManager::GetDevice(uint32_t vmeAddress,
   uint32_t addressModifier, uint32_t dataWidth, uint32_t sizeOfImage)
 {
   LockDevice();
@@ -59,19 +60,19 @@ TUVMEDevice* TUVMEDeviceManager::GetDevice(uint32_t vmeAddress,
   /* If not, we have to insert it into the set.*/
   /* Check to see if the available devices is 0. */
   if (fDevicesRemaining.empty()) {
-    UnlockDevice(); 
+    UnlockDevice();
     /* We have to delete a member. */
     return NULL;
   }
   if (sizeOfImage > fSizePerImage) {
-    UnlockDevice(); 
-    return NULL; 
+    UnlockDevice();
+    return NULL;
   }
   /* Size is too big too handle, try increasing the size of memory allocated
    * to the driver. */
 
   /* There are some empty devices. */
-  /* If the request is for an A16 device, we'll use the 
+  /* If the request is for an A16 device, we'll use the
    * higher granularity images preferentially. */
   uint32_t label;
   while (!fDevicesRemaining.empty()) {
@@ -90,8 +91,8 @@ TUVMEDevice* TUVMEDeviceManager::GetDevice(uint32_t vmeAddress,
       std::set<uint32_t>::iterator setIter;
       for (setIter=fDevicesRemaining.begin();setIter!=fDevicesRemaining.end();setIter++) {
         /* Loops through and tries to find a label that isn't kA16Dev*. */
-        label = *setIter; 
-        if ((label != kA16Dev1) && (label != kA16Dev2)) break; 
+        label = *setIter;
+        if ((label != kA16Dev1) && (label != kA16Dev2)) break;
       }
     }
 
@@ -102,7 +103,7 @@ TUVMEDevice* TUVMEDeviceManager::GetDevice(uint32_t vmeAddress,
       break;
     }
     /* We couldn't open, try to loop through to find one to work. */
-    delete device; 
+    delete device;
     device = NULL;
   }
   if (!device) {
@@ -136,7 +137,7 @@ int32_t TUVMEDeviceManager::CloseDevice(TUVMEDevice* device)
   LockDevice();
 
   if (fAllDevices.find(device) == fAllDevices.end()) {
-    /* Doesn't exist?  This is odd, some dev file is not being 
+    /* Doesn't exist?  This is odd, some dev file is not being
      * handled by the manager.*/
     UnlockDevice();
     return -1;
@@ -152,7 +153,7 @@ int32_t TUVMEDeviceManager::CloseDevice(TUVMEDevice* device)
   return 0;
 }
 
-TUVMEDevice* TUVMEDeviceManager::GetDMADevice(uint32_t vmeAddress, 
+TUVMEDevice* TUVMEDeviceManager::GetDMADevice(uint32_t vmeAddress,
   uint32_t addressModifier, uint32_t dataWidth, bool autoIncrement)
 {
   /* We have to setup the DMA device. */
@@ -165,7 +166,7 @@ TUVMEDevice* TUVMEDeviceManager::GetDMADevice(uint32_t vmeAddress,
   if (fDMADevice.Enable() < 0) return NULL;
   return &fDMADevice;
 }
-    
+
 /************************************************************************/
 /************************************************************************/
 /***************** Define c-wrapped functions. **************************/
@@ -174,7 +175,7 @@ TUVMEDevice* TUVMEDeviceManager::GetDMADevice(uint32_t vmeAddress,
 
 /* We use the device manager singleton class.  This means that two programs cannot be open at the same time. */
 
-TUVMEDevice* get_new_device(uint32_t vmeAddress, uint32_t addressModifier, 
+TUVMEDevice* get_new_device(uint32_t vmeAddress, uint32_t addressModifier,
   uint32_t dataWidth, uint32_t sizeOfImage)
 {
   return TUVMEDeviceManager::GetDeviceManager()->GetDevice(vmeAddress, addressModifier, dataWidth, sizeOfImage);
