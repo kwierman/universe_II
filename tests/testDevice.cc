515101febcf09840ccba119e294f094375f8d1ad
Kevin Wierman <kwierman@gmail.com>
2016-09-02 15:07:40 -0400

    Unit Testing

--- tests/testDevice.cc
+++ tests/testDevice.cc
@@ -7,12 +7,16 @@ int main(){
     std::cout<<"Starting unit tests"<<std::endl;
     std::cout<<"Testing TUVMEDevice"<<std::endl;
 
-    TUVMEDevice mydevice(uint32_t(0));
-    mydevice.Open();
-    std::cout<<"\tDevice Number"<<mydevice.GetDevNumber()<<std::endl;
-    std::cout<<"\tVMEAddress"<<mydevice.GetVMEAddress()<<std::endl;
-    std::cout<<"\tSizeOfImage"<<mydevice.GetSizeOfImage()<<std::endl;
+    for(uint32_t i =0; i<9;i++){
+        std::cout<<"\tTesting Device: "<<i<<std::endl;
+        TUVMEDevice mydevice(i);
+        mydevice.Open();
+        std::cout<<"\tDevice Number: "<<mydevice.GetDevNumber()<<std::endl;
+        std::cout<<"\tVMEAddress: "<<mydevice.GetVMEAddress()<<std::endl;
+        std::cout<<"\tSizeOfImage: "<<mydevice.GetSizeOfImage()<<std::endl;
 
+        std::cout<<std::endl;
+    }
 
     std::cout<<"Successfully Completed"<<std::endl;
 }
